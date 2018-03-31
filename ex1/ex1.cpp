cab047996c1bfa423872e0d50f85305fce360223
SkyFather <cyh278889789@gmail.com>
2018-04-01 01:51:46 +0800

    update ex1

---
+++ ex1/ex1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Howe Chen on 01/04/2018.
+//
+
+#include <cstdio>
+
+int main(int argc, char *argv[]) {
+    puts("Hello World");
+    for (int i = 0; i < 5; ++i) {
+        puts("Hello World");
+    }
+    return 0;
+}
